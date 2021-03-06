6191b8ba12ec9714cadda8d4122cabd5f761677c
Menoita99 <rui.menoita@gmail.com>
2021-03-06 16:20:54 +0000

    added more microservices

---
+++ microservice-security/src/main/java/com/coinpi/cn/financialAPI/security/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.coinpi.cn.financialAPI.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+
+import com.coinpi.cn.financialAPI.security.jwt.JwtAuthenticationFilter;
+import com.coinpi.cn.financialAPI.security.jwt.JwtAuthorizationFilter;
+import com.coinpi.cn.financialAPI.security.jwt.handler.AccessDeniedHandler;
+import com.coinpi.cn.financialAPI.security.jwt.handler.UnauthorizedHandler;
+
+import lombok.Getter;
+
+@Getter
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    private UnauthorizedHandler unauthorizedHandler;
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+    
+	@Autowired
+	@Qualifier("userService")
+	private UserDetailsService userDetailsService;
+
+
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		AuthenticationManager authManager = authenticationManager();
+
+		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+			.and().authorizeRequests()
+			.antMatchers(HttpMethod.GET, JwtAuthenticationFilter.getAuthUrl(), "/api/user/*").permitAll()
+			.antMatchers(HttpMethod.POST,"/cloud/api/user/register/").permitAll()
+			.antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+			.anyRequest().authenticated()
+			.and().csrf().disable()
+			.addFilter(new CorsConfig())
+			.addFilter(new JwtAuthenticationFilter(authManager))
+			.addFilter(new JwtAuthorizationFilter(authManager, userDetailsService))
+			.exceptionHandling()
+			.accessDeniedHandler(accessDeniedHandler)
+			.authenticationEntryPoint(unauthorizedHandler)
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(getEncoder());
+	}
+
+
+
+
+	/**
+	 * @return the encoder
+	 */
+	public static BCryptPasswordEncoder getEncoder() {
+		return encoder;
+	}
+}
---
+++ microservice-service/src/main/java/com/coinpi/cn/financialAPI/security/jwt/handler/UnauthorizedHandler.java
@@ -0,0 +1,32 @@
+package com.coinpi.cn.financialAPI.security.jwt.handler;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.coinpi.cn.financialAPI.security.jwt.ServletUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class UnauthorizedHandler implements AuthenticationEntryPoint {
+    private static Logger logger = LoggerFactory.getLogger(UnauthorizedHandler.class);
+
+    /**
+     * This method is called if user is not authorized
+     */
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    	
+    	logger.warn("UnauthorizedHandler, exception: " + authException);
+
+        String json = ServletUtil.getJson("error", "Not authorized");
+        ServletUtil.write(response, HttpStatus.FORBIDDEN, json);
+    }
+}
