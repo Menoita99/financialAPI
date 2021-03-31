587e90498fbc592aaafbe733ec9b1cee3508e601
Francisco Cardoso <60990445+Franciscodc98@users.noreply.github.com>
2021-03-31 01:31:33 +0100

    commit

--- run_on_docker.bat
+++ run_on_docker.bat
@@ -4,6 +4,6 @@ docker pull menoita99/management_api:latest
 docker pull menoita99/service_api:latest
 docker pull menoita99/security_api:latest
 
-start docker run -t menoita99/management_api:latest
-start docker run -t menoita99/service_api:latest
-start docker run -t menoita99/security_api:latest
+start docker run -p=8080:8080 -t menoita99/management_api:latest 
+start docker run -p=8081:8081 -t menoita99/service_api:latest
+start docker run -p=8082:8082 -t menoita99/security_api:latest
