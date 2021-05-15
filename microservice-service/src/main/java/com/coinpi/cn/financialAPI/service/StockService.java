29d77115e1260664c2fd3060c0c4e1dc9e719b33
Menoita99 <rui.menoita@gmail.com>
2021-05-15 14:22:17 +0100

    update docker file to deploy python service

--- microservice-service/src/main/java/com/coinpi/cn/financialAPI/service/StockService.java
+++ microservice-service/src/main/java/com/coinpi/cn/financialAPI/service/StockService.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
